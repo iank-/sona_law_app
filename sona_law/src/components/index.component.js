2912bfd3bb599e526059e00cc5d466607f8bef3f
iank <ian.k@me.com>
2019-09-14 14:10:04 -0600

    Added router.

---
+++ sona_law/src/components/index.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Index extends Component {
+  render() {
+    return (
+      <div>
+        <p>Welcome to the Index Component!</p>
+      </div>
+    )
+  }
+}
