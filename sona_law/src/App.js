70292a01a1d7bae8af36e0a45e0062a85758b7e2
iank <ian.k@me.com>
2019-09-14 22:26:13 -0600

    More create UI.

--- sona_law/src/App.js
+++ sona_law/src/App.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
-import Create from './components/create.component.js';
-import Edit from './components/edit.component.js';
-import Index from './components/index.component.js';
+import Create from './components/create.component';
+import Edit from './components/edit.component';
+import Index from './components/index.component';
 
 class App extends Component {
 
@@ -12,11 +12,11 @@ class App extends Component {
     return (
       <Router>
         <div className="container">
-          <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <Link to={'/'} className="navbar-brand">Sona Law</Link>
+          <nav className="navbar navbar-expand navbar-light bg-light">
+            <Link to={'/'} className="navbar-brand">Navigation</Link>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
               <ul className="navbar-nav mr-auto">
-                <li className="nav-item">
+              <li className="nav-item">
                   <Link to={'/'} className="nav-link">Home</Link>
                 </li>
                 <li className="nav-item">
@@ -28,11 +28,11 @@ class App extends Component {
               </ul>
             </div>
           </nav> <br/>
-          <h2>Welcome to the tutorial..</h2>
+          <h1>sonaLaw.</h1> <br/>
           <Switch>
-            <Route exact path='/create' component={ Create } />
-            <Route path='/edit/:id' component={ Edit } />
-            <Route path='/index' component={ Index } />
+              <Route exact path='/create' component={ Create } />
+              <Route path='/edit/:id' component={ Edit } />
+              <Route path='/index' component={ Index } />
           </Switch>
         </div>
       </Router>
--- sona_law/src/components/create.component.js
+++ sona_law/src/components/create.component.js
@@ -2,10 +2,71 @@
 import React, { Component } from 'react';
 
 export default class Create extends Component {
+
+  constructor(props) {
+    super(props);
+    this.onChangeClientName = this.onChangeClientName.bind(this);
+    this.onChangeCaseLimitationDate = this.onChangeCaseLimitationDate.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+    this.state = {
+      client_name: '',
+      case_limitation_date: ''
+    }
+  }
+
+  onChangeClientName(e) {
+    this.setState({
+      client_name: e.target.value
+    });
+  }
+
+  onChangeCaseLimitationDate(e) {
+    this.setState({
+      case_limitation_date: e.target.value
+    });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    console.log(`The values are ${this.state.client_name}, ${this.state.case_limitation_date}`)
+
+    this.setState({
+      client_name: '',
+      case_limitation_date: ''
+    });
+  }
+
+
   render() {
     return (
-      <div>
-        <p>Welcome to Create Component!</p>
+      <div style={{marginTop:10}}>
+        <h2>add new client.</h2>
+        <h5>"why don't you just meet me in the miiiiiiiddleeeee?"</h5>
+        <br/>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <label>client name : </label>
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.client_name}
+              onChange={this.onChangeClientName}
+            />
+          </div>
+          <div className="form-group">
+            <label>case limitation date (yyyy/mm/dd): </label>
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.case_limitation_date}
+              onChange={this.onChangeCaseLimitationDate}
+            />
+          </div>
+          <div className="form-group">
+            <input type="submit" value="sign 'em up!" className="btn btn-primary"/>
+          </div>
+        </form>
       </div>
     )
   }
